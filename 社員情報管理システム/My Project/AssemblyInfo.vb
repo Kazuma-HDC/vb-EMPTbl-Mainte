34322b50f330001eafe42d19d1040f3b552b555d
Morita <hdc242313@gmail.com>
2025-05-22 13:49:15 +0900

    Form2～4を追加

---
+++ 社員情報管理システム/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' アセンブリに関する一般情報は以下を通して制御されます
+' アセンブリに関連付けられている情報を変更するには、
+' これらの属性値を変更してください。
+
+' アセンブリ属性の値を確認します
+
+<Assembly: AssemblyTitle("社員情報管理システム")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Dynabook")>
+<Assembly: AssemblyProduct("社員情報管理システム")>
+<Assembly: AssemblyCopyright("Copyright © Dynabook 2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+<Assembly: Guid("6758b822-5bc6-4575-b2d7-1ced7ce20c00")>
+
+' アセンブリのバージョン情報は次の 4 つの値で構成されています:
+'
+'      メジャー バージョン
+'      マイナー バージョン
+'      ビルド番号
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
